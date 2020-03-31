143dfc1fba48ddcbe48775b673a6711d18385f0b
MinhCNN <minhcnnhe130162@fpt.edu.vn>
2020-03-31 20:31:40 +0700

    Test comminting

--- ChangBase.java
+++ ChangBase.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class ChangBase {
 
+	System.out.Println("abc");
+
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
